4c0e13678a48cf30337a8ca3ae6ddf1d657555df
Georg Maißer <georg.maisser@wunderbyte.at>
2023-09-14 15:16:28 +0200

    Improvmenet: Show parentname of entitiy as well as the actually joined entity.

--- classes/table/musi_table.php
+++ classes/table/musi_table.php
@@ -274,7 +274,13 @@ class musi_table extends wunderbyte_table {
 
             $url = new moodle_url('/local/entities/view.php', ['id' => $settings->entity['id']]);
             // Full name of the entity (NOT the shortname).
-            $nametobeshown = $settings->entity['name'];
+
+            if (!empty($settings->entity['parentname'])) {
+                $nametobeshown = $settings->entity['parentname'] . " (" . $settings->entity['name'] . ")";
+            } else {
+                $nametobeshown = $settings->entity['name'];
+            }
+
             return html_writer::tag('a', $nametobeshown, ['href' => $url->out(false)]);
         }
 

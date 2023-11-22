246c2b4804dd7fefeece1cb6b4bb0071a23150a7
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-11-22 18:42:51 +0100

    Improvement: Show all courses page fullscreen.

--- allekurse.php
+++ allekurse.php
@@ -44,7 +44,7 @@ $PAGE->set_url('/local/musi/allekurse.php');
 $PAGE->navbar->add($title);
 $PAGE->set_title(format_string($title));
 $PAGE->set_heading($title);
-$PAGE->set_pagelayout('standard');
+$PAGE->set_pagelayout('base');
 $PAGE->add_body_class('local_musi-allcourses');
 
 echo $OUTPUT->header();

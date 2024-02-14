538cc060fbe587c463e1916a0c471ba145092fa6
Georg Maißer <georg.maisser@wunderbyte.at>
2024-02-14 19:25:27 +0100

    Version 0.8.6 (2024021401)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_musi';
-$plugin->release = '0.8.5';
-$plugin->version = 2024021400;
+$plugin->release = '0.8.6';
+$plugin->version = 2024021401;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
